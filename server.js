01fd13b93297dbe6193222cd1e15a81b3a419874
Barry Yu <doung1118@gmail.com>
2020-06-11 16:12:48 +0800

    Form.js add: axios API

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const path = require('path');
 
 const app = express();
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 
 
 //build static here // 
@@ -95,3 +95,7 @@ app.post('/api/forma', (req, res) => {
 
 
 })
+
+
+
+
