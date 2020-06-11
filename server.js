b494a44047ee3d43a502be3c0ea3be3fae8626ab
Barry Yu <doung1118@gmail.com>
2020-06-11 15:49:06 +0800

    server.js Check port setting , and index.js ReactDOM <app />

--- server.js
+++ server.js
@@ -6,20 +6,18 @@ const nodemailer = require('nodemailer');
 
 
 const cors = require('cors');
-// what is this never ??????????// 
+
 
 
 const path = require('path');
 
 const app = express();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 
 //build static here // 
 app.use(express.static(path.join(__dirname, 'client/build')));
-
-
 
 app.use(bodyParser.json());
 
@@ -32,6 +30,8 @@ app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 
 });
+
+
 
 app.post('/api/forma', (req, res) => {
 
@@ -46,10 +46,52 @@ app.post('/api/forma', (req, res) => {
     }
 
   });
-});
 
 
-app.listen(PORT, () => {
-  console.log("hi")
-  console.log(`server starting at port${PORT}`);
+
+  app.listen(PORT, () => {
+    console.log("hi")
+    console.log(`server starting at port${PORT}`);
+  })
+
+
+
+  let mailOptions = {
+    from: data.email,
+    to: 'doung1118@gmail.com',
+    subject: `message from ${data.name}`,
+    html: `
+
+    <h3>Information </h3>
+    <ul>
+    <li>Name:${data.name}</li>
+    <li>${data.lastname}</li>
+    <li>${data.email}</li>
+     </ul>
+
+     <h3>message</h3>
+     <p>${data.message}</p>
+
+    `
+  };
+
+
+  smtpTransport.sendMail(mailOptions, (error, response) => {
+
+    if (error) {
+      res.send(error)
+    }
+
+    else {
+
+      res.send('success')
+
+    }
+
+  })
+
+
+  smtpTransport.close();
+
+
 })
