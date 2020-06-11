cde76035610f5d79c025ea438d4477c48a66a6d6
Barry Yu <doung1118@gmail.com>
2020-06-11 17:09:44 +0800

    create git heroku

--- server.js
+++ server.js
@@ -8,12 +8,11 @@ const nodemailer = require('nodemailer');
 const cors = require('cors');
 
 
-
 const path = require('path');
 
 const app = express();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 
 //build static here // 
